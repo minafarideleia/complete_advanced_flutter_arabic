8711c01cbf48b968ed7f6826df38d971f2a791dd
minafarid <mebrahim@elm.sa>
2022-02-22 02:24:03 +0300

    Lecture 7 - Creating values Manager

---
+++ lib/presentation/values_manager.dart
@@ -0,0 +1,26 @@
+class AppMargin {
+  static const double m8 = 8.0;
+  static const double m12 = 12.0;
+  static const double m14 = 14.0;
+  static const double m16 = 16.0;
+  static const double m18 = 18.0;
+  static const double m20 = 20.0;
+}
+
+class AppPadding {
+  static const double p8 = 8.0;
+  static const double p12 = 12.0;
+  static const double p14 = 14.0;
+  static const double p16 = 16.0;
+  static const double p18 = 18.0;
+  static const double p20 = 20.0;
+}
+
+class AppSize {
+  static const double s8 = 8.0;
+  static const double s12 = 12.0;
+  static const double s14 = 14.0;
+  static const double s16 = 16.0;
+  static const double s18 = 18.0;
+  static const double s20 = 20.0;
+}
