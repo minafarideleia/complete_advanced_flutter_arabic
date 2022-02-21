5c2c6300c2abe4cb6c296b1d2922bbb8aaeee7a2
minafarid <mebrahim@elm.sa>
2022-02-21 04:39:47 +0300

    Lecture 4 - Creating Application Class

---
+++ lib/app/app.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatefulWidget {
+  // named constructor
+  MyApp._internal();
+
+  int appState = 0;
+
+  static final MyApp _instance =
+      MyApp._internal(); // singleton or single instance
+
+  factory MyApp() => _instance; // factory
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
