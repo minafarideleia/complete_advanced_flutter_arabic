576763131a9b1e6c2ebceb610914cdab32b907d3
minafarid <mebrahim@elm.sa>
2022-02-22 22:52:01 +0300

    Lecture 16 - finish Adding Routes Manager

--- lib/presentation/resources/routes_manager.dart
+++ lib/presentation/resources/routes_manager.dart
@@ -1,3 +1,13 @@
+import 'dart:html';
+
+import 'package:advanced_flutter_arabic/presentation/forgot_password/forgot_password_view.dart';
+import 'package:advanced_flutter_arabic/presentation/login/login_view.dart';
+import 'package:advanced_flutter_arabic/presentation/main/main_view.dart';
+import 'package:advanced_flutter_arabic/presentation/register/register_view.dart';
+import 'package:advanced_flutter_arabic/presentation/splash/splash_view.dart';
+import 'package:advanced_flutter_arabic/presentation/store_details/store_details_view.dart';
+import 'package:flutter/material.dart';
+
 class Routes {
   static const String splashRoute = "/";
   static const String loginRoute = "/login";
@@ -5,4 +15,38 @@ class Routes {
   static const String forgotPasswordRoute = "/forgotPassword";
   static const String mainRoute = "/main";
   static const String storeDetailsRoute = "/storeDetails";
+}
+
+class RouteGenerator {
+  static Route<dynamic> getRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case Routes.splashRoute:
+        return MaterialPageRoute(builder: (_) => const SplashView());
+      case Routes.loginRoute:
+        return MaterialPageRoute(builder: (_) => const LoginView());
+      case Routes.registerRoute:
+        return MaterialPageRoute(builder: (_) => const RegisterView());
+      case Routes.forgotPasswordRoute:
+        return MaterialPageRoute(builder: (_) => const ForgotPasswordView());
+      case Routes.mainRoute:
+        return MaterialPageRoute(builder: (_) => const MainView());
+      case Routes.storeDetailsRoute:
+        return MaterialPageRoute(builder: (_) => const StoreDetailsView());
+      default:
+        return unDefinedRoute();
+    }
+  }
+
+  static Route<dynamic> unDefinedRoute() {
+    return MaterialPageRoute(
+        builder: (_) => Scaffold(
+              appBar: AppBar(
+                title: const Text(
+                    "No Route Found"), // todo move this string to strings manager
+              ),
+              body: const Center(
+                  child: Text(
+                      "No Route Found")), // todo move this string to strings manager
+            ));
+  }
 }
