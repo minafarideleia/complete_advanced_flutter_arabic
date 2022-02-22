8f32a7a5e060eeec29cca80e13ae4687f3480a84
minafarid <mebrahim@elm.sa>
2022-02-22 05:05:35 +0300

    Lecture 14 - Adding text form field theme

--- lib/presentation/theme_manager.dart
+++ lib/presentation/theme_manager.dart
@@ -53,5 +53,38 @@ ThemeData getApplicationTheme() {
         caption: getRegularStyle(color: ColorManager.grey1),
         bodyText1: getRegularStyle(color: ColorManager.grey)),
     // input decoration theme (text form field)
+    inputDecorationTheme: InputDecorationTheme(
+        // content padding
+        contentPadding: const EdgeInsets.all(AppPadding.p8),
+        // hint style
+        hintStyle:
+            getRegularStyle(color: ColorManager.grey, fontSize: FontSize.s14),
+        labelStyle:
+            getMediumStyle(color: ColorManager.grey, fontSize: FontSize.s14),
+        errorStyle: getRegularStyle(color: ColorManager.error),
+
+        // enabled border style
+        enabledBorder: OutlineInputBorder(
+            borderSide:
+                BorderSide(color: ColorManager.primary, width: AppSize.s1_5),
+            borderRadius: const BorderRadius.all(Radius.circular(AppSize.s8))),
+
+        // focused border style
+        focusedBorder: OutlineInputBorder(
+            borderSide:
+                BorderSide(color: ColorManager.grey, width: AppSize.s1_5),
+            borderRadius: const BorderRadius.all(Radius.circular(AppSize.s8))),
+
+        // error border style
+        errorBorder: OutlineInputBorder(
+            borderSide:
+                BorderSide(color: ColorManager.error, width: AppSize.s1_5),
+            borderRadius: const BorderRadius.all(Radius.circular(AppSize.s8))),
+        // focused border style
+        focusedErrorBorder: OutlineInputBorder(
+            borderSide:
+                BorderSide(color: ColorManager.primary, width: AppSize.s1_5),
+            borderRadius: const BorderRadius.all(Radius.circular(AppSize.s8)))),
+    // label style
   );
 }
