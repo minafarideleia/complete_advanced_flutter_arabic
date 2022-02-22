5b145e1bd23845ef11de32bcfd0f70623df53db7
minafarid <mebrahim@elm.sa>
2022-02-22 04:28:38 +0300

    Lecture 13 - Adding text theme

--- lib/presentation/theme_manager.dart
+++ lib/presentation/theme_manager.dart
@@ -6,44 +6,52 @@ import 'package:flutter/material.dart';
 
 ThemeData getApplicationTheme() {
   return ThemeData(
-      // main colors
-      primaryColor: ColorManager.primary,
-      primaryColorLight: ColorManager.lightPrimary,
-      primaryColorDark: ColorManager.darkPrimary,
-      disabledColor: ColorManager.grey1,
-      splashColor: ColorManager.lightPrimary,
-      // ripple effect color
-      // cardview theme
-      cardTheme: CardTheme(
-          color: ColorManager.white,
-          shadowColor: ColorManager.grey,
-          elevation: AppSize.s4),
-      // app bar theme
-      appBarTheme: AppBarTheme(
-          centerTitle: true,
-          color: ColorManager.primary,
-          elevation: AppSize.s4,
-          shadowColor: ColorManager.lightPrimary,
-          titleTextStyle: getRegularStyle(
-              fontSize: FontSize.s16, color: ColorManager.white)),
-      // button theme
-      buttonTheme: ButtonThemeData(
-          shape: const StadiumBorder(),
-          disabledColor: ColorManager.grey1,
-          buttonColor: ColorManager.primary,
-          splashColor: ColorManager.lightPrimary),
+    // main colors
+    primaryColor: ColorManager.primary,
+    primaryColorLight: ColorManager.lightPrimary,
+    primaryColorDark: ColorManager.darkPrimary,
+    disabledColor: ColorManager.grey1,
+    splashColor: ColorManager.lightPrimary,
+    // ripple effect color
+    // cardview theme
+    cardTheme: CardTheme(
+        color: ColorManager.white,
+        shadowColor: ColorManager.grey,
+        elevation: AppSize.s4),
+    // app bar theme
+    appBarTheme: AppBarTheme(
+        centerTitle: true,
+        color: ColorManager.primary,
+        elevation: AppSize.s4,
+        shadowColor: ColorManager.lightPrimary,
+        titleTextStyle:
+            getRegularStyle(fontSize: FontSize.s16, color: ColorManager.white)),
+    // button theme
+    buttonTheme: ButtonThemeData(
+        shape: const StadiumBorder(),
+        disabledColor: ColorManager.grey1,
+        buttonColor: ColorManager.primary,
+        splashColor: ColorManager.lightPrimary),
 
-      // elevated button them
-      elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-              textStyle: getRegularStyle(
-                  color: ColorManager.white, fontSize: FontSize.s17),
-              primary: ColorManager.primary,
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(AppSize.s12))))
+    // elevated button them
+    elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+            textStyle: getRegularStyle(
+                color: ColorManager.white, fontSize: FontSize.s17),
+            primary: ColorManager.primary,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(AppSize.s12)))),
 
-      // text theme
-
-      // input decoration theme (text form field)
-      );
+    // text theme
+    textTheme: TextTheme(
+        displayLarge:
+            getLightStyle(color: ColorManager.white, fontSize: FontSize.s22),
+        headline1: getSemiBoldStyle(
+            color: ColorManager.darkGrey, fontSize: FontSize.s16),
+        subtitle1: getMediumStyle(
+            color: ColorManager.lightGrey, fontSize: FontSize.s14),
+        caption: getRegularStyle(color: ColorManager.grey1),
+        bodyText1: getRegularStyle(color: ColorManager.grey)),
+    // input decoration theme (text form field)
+  );
 }
