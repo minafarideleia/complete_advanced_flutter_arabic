dfb0177b589432b8bfa7883945596bd39de38b73
minafarid <mebrahim@elm.sa>
2022-02-23 04:08:21 +0300

    Lecture 22 adding assets for onboarding screen

--- lib/presentation/resources/strings_manager.dart
+++ lib/presentation/resources/strings_manager.dart
@@ -1,3 +1,16 @@
-class AppStrings{
+class AppStrings {
   static const noRouteFound = "No Route Found";
+  static const onBoardingTitle1 = "SEE THE BEST COURSE #1";
+  static const onBoardingTitle2 = "SEE THE BEST COURSE #2";
+  static const onBoardingTitle3 = "SEE THE BEST COURSE #3";
+  static const onBoardingTitle4 = "SEE THE BEST COURSE #4";
+
+  static const onBoardingSubTitle1 =
+      "Tut app is an awesome flutter application using clean architecture #1";
+  static const onBoardingSubTitle2 =
+      "Tut app is an awesome flutter application using clean architecture #2";
+  static const onBoardingSubTitle3 =
+      "Tut app is an awesome flutter application using clean architecture #3";
+  static const onBoardingSubTitle4 =
+      "Tut app is an awesome flutter application using clean architecture #4";
 }
