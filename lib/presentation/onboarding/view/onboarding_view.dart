9cbc0c6600eeb4701be1e064d5b23497d093fdd0
minafarid <mebrahim@elm.sa>
2022-03-04 22:58:44 +0300

    Lecture 35 Continue Onboarding Viewmodel Logic Implementation

--- lib/presentation/onboarding/view/onboarding_view.dart
+++ lib/presentation/onboarding/view/onboarding_view.dart
@@ -18,20 +18,7 @@ class OnBoardingView extends StatefulWidget {
 }
 
 class _OnBoardingViewState extends State<OnBoardingView> {
-  late final List<SliderObject> _list = _getSliderData();
   final PageController _pageController = PageController();
-  int _currentIndex = 0;
-
-  List<SliderObject> _getSliderData() => [
-        SliderObject(AppStrings.onBoardingTitle1,
-            AppStrings.onBoardingSubTitle1, ImageAssets.onboardingLogo1),
-        SliderObject(AppStrings.onBoardingTitle2,
-            AppStrings.onBoardingSubTitle2, ImageAssets.onboardingLogo2),
-        SliderObject(AppStrings.onBoardingTitle3,
-            AppStrings.onBoardingSubTitle3, ImageAssets.onboardingLogo3),
-        SliderObject(AppStrings.onBoardingTitle4,
-            AppStrings.onBoardingSubTitle4, ImageAssets.onboardingLogo4),
-      ];
 
   @override
   Widget build(BuildContext context) {
@@ -163,6 +150,12 @@ class _OnBoardingViewState extends State<OnBoardingView> {
     } else {
       return SvgPicture.asset(ImageAssets.solidCircleIc);
     }
+  }
+
+  @override
+  void dispose() {
+    // TODO: viewmodel.dispose()
+    super.dispose();
   }
 }
 
