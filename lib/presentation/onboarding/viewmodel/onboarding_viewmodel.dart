f758b44e30b17ac7cfd1b51a96a20ae37190c04e
minafarid <mebrahim@elm.sa>
2022-03-04 22:13:36 +0300

    Lecture 34 Adding Stream Controller to Onboarding Viewmodel

--- lib/presentation/onboarding/viewmodel/onboarding_viewmodel.dart
+++ lib/presentation/onboarding/viewmodel/onboarding_viewmodel.dart
@@ -1,8 +1,13 @@
+import 'dart:async';
+
+import 'package:advanced_flutter_arabic/domain/models.dart';
+
 import '../../base/baseviewmodel.dart';
 
-class OnBoardingViewModel extends BaseViewModel with OnBoardingViewModelInputs,OnBoardingViewModelOutputs{
+class OnBoardingViewModel extends BaseViewModel
+    with OnBoardingViewModelInputs, OnBoardingViewModelOutputs {
   // stream controllers outputs
-
+  StreamController _streamController = StreamController<SliderViewObject>();
 
   //OnBoarding ViewModel Inputs
   @override
@@ -30,6 +35,15 @@ class OnBoardingViewModel extends BaseViewModel with OnBoardingViewModelInputs,O
   void onPageChanged(int index) {
     // TODO: implement onPageChanged
   }
+
+  @override
+  // TODO: implement inputSliderViewObject
+  Sink get inputSliderViewObject => throw UnimplementedError();
+
+  //OnBoarding ViewModel outputs
+  @override
+  // TODO: implement outputSliderViewObject
+  Stream get outputSliderViewObject => throw UnimplementedError();
 }
 
 // inputs mean that "Orders" that our view model will receive from view
@@ -37,6 +51,12 @@ abstract class OnBoardingViewModelInputs {
   void goNext(); // when user clicks on right arrow or swipe left
   void goPrevious(); // when user clicks on left arrow or swipe right
   void onPageChanged(int index);
+
+  // stream controller input
+  Sink get inputSliderViewObject;
 }
 
-abstract class OnBoardingViewModelOutputs {}
+abstract class OnBoardingViewModelOutputs {
+  // stream controller output
+  Stream get outputSliderViewObject;
+}
