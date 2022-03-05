2ded444e63c08d77aef5bcff73af87e24f8d0341
minafarid <mebrahim@elm.sa>
2022-03-05 06:26:07 +0300

    Lecture 45 Data Layer Add API Service Client Retrofi

---
+++ lib/data/network/app_api.dart
@@ -0,0 +1,16 @@
+import 'package:advanced_flutter_arabic/data/response/responses.dart';
+import 'package:dio/dio.dart';
+import 'package:retrofit/http.dart';
+
+import '../../app/constants.dart';
+
+part 'app_api.g.dart';
+
+@RestApi(baseUrl: Constants.baseUrl)
+abstract class AppServiceClient {
+  factory AppServiceClient(Dio dio, {String baseUrl}) = _AppServiceClient;
+
+  @POST("/customers/login")
+  Future<AuthenticationResponse> login(
+      @Field("email") String email, @Field("password") String password);
+}
---
+++ lib/data/network/app_api.g.dart
@@ -0,0 +1,48 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'app_api.dart';
+
+// **************************************************************************
+// RetrofitGenerator
+// **************************************************************************
+
+// ignore_for_file: unnecessary_brace_in_string_interps
+
+class _AppServiceClient implements AppServiceClient {
+  _AppServiceClient(this._dio, {this.baseUrl}) {
+    baseUrl ??= 'http://minafarid246.mocklab.io';
+  }
+
+  final Dio _dio;
+
+  String? baseUrl;
+
+  @override
+  Future<AuthenticationResponse> login(email, password) async {
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    final _data = {'email': email, 'password': password};
+    final _result = await _dio.fetch<Map<String, dynamic>>(
+        _setStreamType<AuthenticationResponse>(
+            Options(method: 'POST', headers: _headers, extra: _extra)
+                .compose(_dio.options, '/customers/login',
+                    queryParameters: queryParameters, data: _data)
+                .copyWith(baseUrl: baseUrl ?? _dio.options.baseUrl)));
+    final value = AuthenticationResponse.fromJson(_result.data!);
+    return value;
+  }
+
+  RequestOptions _setStreamType<T>(RequestOptions requestOptions) {
+    if (T != dynamic &&
+        !(requestOptions.responseType == ResponseType.bytes ||
+            requestOptions.responseType == ResponseType.stream)) {
+      if (T == String) {
+        requestOptions.responseType = ResponseType.plain;
+      } else {
+        requestOptions.responseType = ResponseType.json;
+      }
+    }
+    return requestOptions;
+  }
+}
