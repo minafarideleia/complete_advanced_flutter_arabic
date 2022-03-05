3136686898cdf21f50a0209323f3cc0cdaf0327c
minafarid <mebrahim@elm.sa>
2022-03-05 03:51:07 +0300

    Lecture 43 Data Layer Adding APIs Responses

---
+++ lib/data/responses.dart
@@ -0,0 +1,36 @@
+import 'package:json_annotation/json_annotation.dart';
+
+@JsonSerializable()
+class BaseResponse {
+  @JsonKey(name: "status")
+  int? status;
+  @JsonKey(name: "message")
+  String? message;
+}
+
+@JsonSerializable()
+class CustomerResponse {
+  @JsonKey(name: "id")
+  String? id;
+  @JsonKey(name: "name")
+  String? name;
+  @JsonKey(name: "numOfNotifications")
+  int? numOfNotifications;
+}
+
+@JsonSerializable()
+class ContactsResponse {
+  @JsonKey(name: "phone")
+  String? phone;
+  @JsonKey(name: "email")
+  String? email;
+  @JsonKey(name: "link")
+  String? link;
+}
+
+class AuthenticationResponse extends BaseResponse{
+  @JsonKey(name: "customer")
+  CustomerResponse? customer;
+  @JsonKey(name: "contacts")
+  ContactsResponse? contacts;
+}
