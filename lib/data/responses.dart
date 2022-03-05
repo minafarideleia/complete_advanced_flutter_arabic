43c9a584d71b82debfddf7154b6b0193c4b0fffe
minafarid <mebrahim@elm.sa>
2022-03-05 04:41:01 +0300

    revert changes

--- lib/data/responses.dart
+++ lib/data/responses.dart
@@ -1,7 +1,5 @@
 import 'package:json_annotation/json_annotation.dart';
 
-part 'responses.g.dart';
-
 @JsonSerializable()
 class BaseResponse {
   @JsonKey(name: "status")
@@ -18,15 +16,6 @@ class CustomerResponse {
   String? name;
   @JsonKey(name: "numOfNotifications")
   int? numOfNotifications;
-
-  CustomerResponse(this.id, this.name, this.numOfNotifications);
-
-  // from json
-  factory CustomerResponse.fromJson(Map<String, dynamic> json) =>
-      _$CustomerResponseFromJson(json);
-
-// to json
-  Map<String, dynamic> toJson() => _$CustomerResponseToJson(this);
 }
 
 @JsonSerializable()
@@ -37,30 +26,11 @@ class ContactsResponse {
   String? email;
   @JsonKey(name: "link")
   String? link;
-
-  ContactsResponse(this.phone, this.email, this.link);
-
-  // from json
-  factory ContactsResponse.fromJson(Map<String, dynamic> json) =>
-      _$ContactsResponseFromJson(json);
-
-// to json
-  Map<String, dynamic> toJson() => _$ContactsResponseToJson(this);
 }
 
-@JsonSerializable()
-class AuthenticationResponse extends BaseResponse {
+class AuthenticationResponse extends BaseResponse{
   @JsonKey(name: "customer")
   CustomerResponse? customer;
   @JsonKey(name: "contacts")
   ContactsResponse? contacts;
-
-  AuthenticationResponse(this.customer, this.contacts);
-
-  // from json
-  factory AuthenticationResponse.fromJson(Map<String, dynamic> json) =>
-      _$AuthenticationResponseFromJson(json);
-
-// to json
-  Map<String, dynamic> toJson() => _$AuthenticationResponseToJson(this);
 }
