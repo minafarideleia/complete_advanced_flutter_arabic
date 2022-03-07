e75464935871301cb3d50500b03b9b04f384323b
minafarid <mebrahim@elm.sa>
2022-03-07 23:53:09 +0300

    Lecture 56 Data Layer Adding Responses Messages

--- lib/data/network/error_handler.dart
+++ lib/data/network/error_handler.dart
@@ -14,7 +14,7 @@ enum DataSource {
   NO_INTERNET_CONNECTION
 }
 
-class ResponseCode{
+class ResponseCode {
   static const int SUCCESS = 200; // success with data
   static const int NO_CONTENT = 201; // success with no data (no content)
   static const int BAD_REQUEST = 400; // failure, API rejected request
@@ -23,11 +23,35 @@ class ResponseCode{
   static const int INTERNAL_SERVER_ERROR = 500; // failure, crash in server side
 
   // local status code
-  static const int CONNECT_TIMEOUT = -1; //
-  static const int CANCEL = -2; //
-  static const int RECIEVE_TIMEOUT = -3; //
-  static const int SEND_TIMEOUT = -4; //
-  static const int CACHE_ERROR = -5; //
-  static const int NO_INTERNET_CONNECTION = -6; //
-  static const int UNKNOWN = -7; //
+  static const int CONNECT_TIMEOUT = -1;
+  static const int CANCEL = -2;
+  static const int RECIEVE_TIMEOUT = -3;
+  static const int SEND_TIMEOUT = -4;
+  static const int CACHE_ERROR = -5;
+  static const int NO_INTERNET_CONNECTION = -6;
+  static const int UNKNOWN = -7;
+}
+
+class ResponseMessage {
+  static const String SUCCESS = "success"; // success with data
+  static const String NO_CONTENT =
+      "success"; // success with no data (no content)
+  static const String BAD_REQUEST =
+      "Bad request, Try again later"; // failure, API rejected request
+  static const String UNAUTORISED =
+      "User is unauthorised, Try again later"; // failure, user is not authorised
+  static const String FORBIDDEN =
+      "Forbidden request, Try again later"; //  failure, API rejected request
+  static const String INTERNAL_SERVER_ERROR =
+      "Some thing went wrong, Try again later"; // failure, crash in server side
+
+  // local status code
+  static const String CONNECT_TIMEOUT = "Time out error, Try again later";
+  static const String CANCEL = "Request was cancelled, Try again later";
+  static const String RECIEVE_TIMEOUT = "Time out error, Try again later";
+  static const String SEND_TIMEOUT = "Time out error, Try again later";
+  static const String CACHE_ERROR = "Cache error, Try again later";
+  static const String NO_INTERNET_CONNECTION =
+      "Please check your internet connection";
+  static const String UNKNOWN = "Some thing went wrong, Try again later";
 }
