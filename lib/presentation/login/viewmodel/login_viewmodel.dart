32fb25a9f7c272b43793e1d0ab4dbb2b23fc960b
minafarid <mebrahim@elm.sa>
2022-03-10 03:58:03 +0300

    Lecture 71 - Adding Login UseCase to Login ViewModel

--- lib/presentation/login/viewmodel/login_viewmodel.dart
+++ lib/presentation/login/viewmodel/login_viewmodel.dart
@@ -2,17 +2,20 @@ import 'dart:async';
 
 import 'package:advanced_flutter_arabic/presentation/base/baseviewmodel.dart';
 
+import '../../../domain/usecase/login_usecase.dart';
 import '../../common/freezed_data_classes.dart';
 
 class LoginViewModel extends BaseViewModel
     with LoginViewModelInputs, LoginViewModelOutputs {
   final StreamController _userNameStreamController =
-      StreamController<String>.broadcast();
+  StreamController<String>.broadcast();
   final StreamController _passwordStreamController =
-      StreamController<String>.broadcast();
+  StreamController<String>.broadcast();
 
-  var loginObject = LoginObject("","");
+  var loginObject = LoginObject("", "");
+  final LoginUseCase _loginUseCase;
 
+  LoginViewModel(this._loginUseCase);
 
   // inputs
   @override
@@ -45,19 +48,30 @@ class LoginViewModel extends BaseViewModel
   }
 
   @override
-  login() {
-    // TODO: implement login
-    throw UnimplementedError();
+  login() async {
+    (await _loginUseCase.execute(
+        LoginUseCaseInput(loginObject.userName, loginObject.password)))
+        .fold((failure) => {
+          // left -> failure
+      print(failure.message)
+
+    }, (data) => {
+          // right -> data (success)
+      print(data.customer?.name)
+
+    });
   }
 
   // outputs
   @override
-  Stream<bool> get outIsPasswordValid => _passwordStreamController.stream
-      .map((password) => _isPasswordValid(password));
+  Stream<bool> get outIsPasswordValid =>
+      _passwordStreamController.stream
+          .map((password) => _isPasswordValid(password));
 
   @override
-  Stream<bool> get outIsUserNameValid => _userNameStreamController.stream
-      .map((userName) => _isUserNameValid(userName));
+  Stream<bool> get outIsUserNameValid =>
+      _userNameStreamController.stream
+          .map((userName) => _isUserNameValid(userName));
 
   bool _isPasswordValid(String password) {
     return password.isNotEmpty;
