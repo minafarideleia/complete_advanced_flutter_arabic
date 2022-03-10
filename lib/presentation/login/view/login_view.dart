d47a9a361304cb86296175c5a02300bc0ae85ffd
minafarid <mebrahim@elm.sa>
2022-03-11 02:05:54 +0300

    Lecture 73 - Login View UI

--- lib/presentation/login/view/login_view.dart
+++ lib/presentation/login/view/login_view.dart
@@ -1,5 +1,9 @@
 import 'package:advanced_flutter_arabic/presentation/login/viewmodel/login_viewmodel.dart';
+import 'package:advanced_flutter_arabic/presentation/resources/color_manager.dart';
+import 'package:advanced_flutter_arabic/presentation/resources/values_manager.dart';
 import 'package:flutter/material.dart';
+
+import '../../resources/assets_manager.dart';
 
 class LoginView extends StatefulWidget {
   const LoginView({Key? key}) : super(key: key);
@@ -12,6 +16,7 @@ class _LoginViewState extends State<LoginView> {
   final LoginViewModel _viewModel = LoginViewModel(_loginUseCase);
   final TextEditingController _userNameController = TextEditingController();
   final TextEditingController _userPasswordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
 
   _bind() {
     _viewModel.start(); // tell viewmodel, start ur job
@@ -30,6 +35,40 @@ class _LoginViewState extends State<LoginView> {
   @override
   Widget build(BuildContext context) {
     return Container();
+  }
+
+  Widget _getContentWidget() {
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.only(top: AppPadding.p100),
+        color: ColorManager.white,
+        child: SingleChildScrollView(
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                const Center(
+                    child: Image(image: AssetImage(ImageAssets.splashLogo))),
+                const SizedBox(
+                  height: AppSize.s28,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(
+                      left: AppPadding.p28, right: AppPadding.p28),
+                  child: StreamBuilder<bool>(
+                      stream: _viewModel.outIsUserNameValid,
+                      builder: (context, snapshot) {
+                        return TextFormField(
+                            keyboardType: TextInputType.emailAddress,
+                            controller: _userNameController);
+                      }),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 
   @override
