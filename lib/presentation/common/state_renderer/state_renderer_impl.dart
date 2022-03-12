fdb27cc7eb51c270323547c41c6e3e79be32f494
minafarid <mebrahim@elm.sa>
2022-03-12 23:59:45 +0300

    Lecture 94 - Applying State Renderer - Loading State

---
+++ lib/presentation/common/state_renderer/state_renderer_impl.dart
@@ -0,0 +1,26 @@
+import 'dart:html';
+
+import 'package:advanced_flutter_arabic/presentation/common/state_renderer/state_renderer.dart';
+
+import '../../resources/strings_manager.dart';
+
+abstract class FlowState {
+  StateRendererType getStateRendererType();
+
+  String getMessage();
+}
+// loading state (POPUP,FULL SCREEN)
+
+class LoadingState extends FlowState {
+  StateRendererType stateRendererType;
+  String? message;
+
+  LoadingState(
+      {required this.stateRendererType, String message = AppStrings.loading});
+
+  @override
+  String getMessage() => message ?? AppStrings.loading;
+
+  @override
+  StateRendererType getStateRendererType() => stateRendererType;
+}
