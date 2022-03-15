75cef65a4e27cd845fa613ef4f6fe47291bf4c2d
minafarid <mebrahim@elm.sa>
2022-03-15 03:49:23 +0300

    Lecture 114 Add Register API Call in Repository Impl, Remote Data Source and Usecase

--- lib/data/repository/repository_impl.dart
+++ lib/data/repository/repository_impl.dart
@@ -73,4 +73,32 @@ class RepositoryImpl implements Repository {
       return Left(DataSource.NO_INTERNET_CONNECTION.getFailure());
     }
   }
+
+  @override
+  Future<Either<Failure, Authentication>> register(RegisterRequest registerRequest) async {
+    if (await _networkInfo.isConnected) {
+      // its connected to internet, its safe to call API
+      try {
+        final response = await _remoteDataSource.register(registerRequest);
+
+        if (response.status == ApiInternalStatus.SUCCESS) {
+          // success
+          // return either right
+          // return data
+          return Right(response.toDomain());
+        } else {
+          // failure --return business error
+          // return either left
+          return Left(Failure(ApiInternalStatus.FAILURE,
+              response.message ?? ResponseMessage.DEFAULT));
+        }
+      } catch (error) {
+        return Left(ErrorHandler.handle(error).failure);
+      }
+    } else {
+      // return internet connection error
+      // return either left
+      return Left(DataSource.NO_INTERNET_CONNECTION.getFailure());
+    }
+  }
 }
---
+++ lib/domain/usecase/register_usecase.dart
@@ -0,0 +1,38 @@
+import 'package:advanced_flutter_arabic/data/network/failure.dart';
+import 'package:advanced_flutter_arabic/data/network/requests.dart';
+import 'package:advanced_flutter_arabic/domain/model/models.dart';
+import 'package:advanced_flutter_arabic/domain/usecase/base_usecase.dart';
+import 'package:dartz/dartz.dart';
+
+import '../repository/repository.dart';
+
+class RegisterUseCase
+    implements BaseUseCase<RegisterUseCaseInput, Authentication> {
+  final Repository _repository;
+
+  RegisterUseCase(this._repository);
+
+  @override
+  Future<Either<Failure, Authentication>> execute(
+      RegisterUseCaseInput input) async {
+    return await _repository.register(RegisterRequest(
+        input.userName,
+        input.countryMobileCode,
+        input.mobileNumber,
+        input.email,
+        input.password,
+        input.profilePicture));
+  }
+}
+
+class RegisterUseCaseInput {
+  String userName;
+  String countryMobileCode;
+  String mobileNumber;
+  String email;
+  String password;
+  String profilePicture;
+
+  RegisterUseCaseInput(this.userName, this.countryMobileCode, this.mobileNumber,
+      this.email, this.password, this.profilePicture);
+}
