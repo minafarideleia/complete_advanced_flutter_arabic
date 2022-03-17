9c53107473c6ee4c921baadbcd514c2ad0df5533
minafarid <mebrahim@elm.sa>
2022-03-18 02:19:30 +0300

    Lecture 126 - Adding UI for Register Screen Part 2

--- lib/presentation/resources/strings_manager.dart
+++ lib/presentation/resources/strings_manager.dart
@@ -29,9 +29,10 @@ class AppStrings {
   static const String resetPassword = "Reset Password";
   static const String success = "Success";
   static const String userNameInvalid = "User name should be at least 8 chars";
-  static const String mobileNumberInvalid = "Mobile should be at least 10 digits";
+  static const String mobileNumberInvalid =
+      "Mobile should be at least 10 digits";
   static const String passwordInvalid = "Password should be at least 6 chars";
   static const String alreadyHaveAccount = "Already have an account ? Login";
   static const register = "Register";
-
+  static const mobileNumber = "Mobile Number";
 }
