4fbd0c4d7461ac5a0646bf58a94ac851059b9715
minafarid <mebrahim@elm.sa>
2022-03-18 08:00:22 +0300

    Lecture 139 - Adding Home API Models

--- lib/domain/model/models.dart
+++ lib/domain/model/models.dart
@@ -41,3 +41,42 @@ class Authentication {
 
   Authentication(this.customer, this.contacts);
 }
+
+class Service {
+  String id;
+  String title;
+  String image;
+
+  Service(this.id, this.title, this.image);
+}
+
+class Store {
+  String id;
+  String title;
+  String image;
+
+  Store(this.id, this.title, this.image);
+}
+
+class BannerAd {
+  String id;
+  String title;
+  String image;
+  String link;
+
+  BannerAd(this.id, this.title, this.image, this.link);
+}
+
+class HomeData {
+  List<Service> services;
+  List<BannerAd> banners;
+  List<Store> stores;
+
+  HomeData(this.services, this.banners, this.stores);
+}
+
+class HomeObject {
+  HomeData? data;
+
+  HomeObject(this.data);
+}
