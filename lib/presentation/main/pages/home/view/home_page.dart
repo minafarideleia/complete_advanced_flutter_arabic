5362f61aebfd140ca0ea36afa954216e6b9a5281
minafarid <mebrahim@elm.sa>
2022-03-18 22:34:50 +0300

    Lecture 150 - Adding Banner Carousel Widget

--- lib/presentation/main/pages/home/view/home_page.dart
+++ lib/presentation/main/pages/home/view/home_page.dart
@@ -1,7 +1,10 @@
+import 'package:advanced_flutter_arabic/domain/model/models.dart';
 import 'package:advanced_flutter_arabic/presentation/common/state_renderer/state_renderer_impl.dart';
 import 'package:advanced_flutter_arabic/presentation/main/pages/home/viewmodel/home_viewmodel.dart';
+import 'package:advanced_flutter_arabic/presentation/resources/color_manager.dart';
 import 'package:advanced_flutter_arabic/presentation/resources/strings_manager.dart';
 import 'package:advanced_flutter_arabic/presentation/resources/values_manager.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 import '../../../../../app/di.dart';
@@ -57,7 +60,40 @@ class _HomePageState extends State<HomePage> {
   }
 
   Widget _getBannersCarousel() {
-    return Center();
+    return StreamBuilder<List<BannerAd>>(
+        stream: _viewModel.outputBanners,
+        builder: (context, snapshot) {
+          return _getBannerWidget(snapshot.data);
+        });
+  }
+
+  Widget _getBannerWidget(List<BannerAd>? banners) {
+    if (banners != null) {
+      return CarouselSlider(
+          items: banners
+              .map((banner) => SizedBox(
+                    width: double.infinity,
+                    child: Card(
+                      elevation: AppSize.s1_5,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(AppSize.s12),
+                          side: BorderSide(
+                              color: ColorManager.primary, width: AppSize.s1)),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(AppSize.s12),
+                        child: Image.network(banner.image, fit: BoxFit.cover),
+                      ),
+                    ),
+                  ))
+              .toList(),
+          options: CarouselOptions(
+              height: AppSize.s90,
+              autoPlay: true,
+              enableInfiniteScroll: true,
+              enlargeCenterPage: true));
+    } else {
+      return Container();
+    }
   }
 
   Widget _getSection(String title) {
