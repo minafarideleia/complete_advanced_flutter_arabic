16d8812849b512f3a326af74e11ce3a3b9ba23e1
minafarid <mebrahim@elm.sa>
2022-03-18 09:21:14 +0300

    Lecture 148 add Home View, Adding Dependency Injection and Binding View to ViewModel

--- lib/presentation/main/pages/home/view/home_page.dart
+++ lib/presentation/main/pages/home/view/home_page.dart
@@ -1,5 +1,8 @@
+import 'package:advanced_flutter_arabic/presentation/main/pages/home/viewmodel/home_viewmodel.dart';
 import 'package:advanced_flutter_arabic/presentation/resources/strings_manager.dart';
 import 'package:flutter/material.dart';
+
+import '../../../../../app/di.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -9,10 +12,28 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final HomeViewModel _viewModel = instance<HomeViewModel>();
+
+  _bind() {
+    _viewModel.start();
+  }
+
+  @override
+  void initState() {
+    _bind();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Center(
       child: Text(AppStrings.home),
     );
+  }
+
+  @override
+  void dispose() {
+    _viewModel.dispose();
+    super.dispose();
   }
 }
