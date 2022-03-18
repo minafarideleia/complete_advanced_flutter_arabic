31777492638d43a167906e11540b6dab018d8152
minafarid <mebrahim@elm.sa>
2022-03-18 09:12:30 +0300

    Lecture 147 - Adding Implementation of Home API in ViewModel

--- lib/presentation/main/pages/home/viewmodel/home_viewmodel.dart
+++ lib/presentation/main/pages/home/viewmodel/home_viewmodel.dart
@@ -1,9 +1,13 @@
 import 'dart:async';
+import 'dart:ffi';
 
 import 'package:advanced_flutter_arabic/domain/model/models.dart';
 import 'package:advanced_flutter_arabic/domain/usecase/home_usecase.dart';
 import 'package:advanced_flutter_arabic/presentation/base/baseviewmodel.dart';
 import 'package:rxdart/rxdart.dart';
+
+import '../../../../common/state_renderer/state_renderer.dart';
+import '../../../../common/state_renderer/state_renderer_impl.dart';
 
 class HomeViewModel extends BaseViewModel
     with HomeViewModelInput, HomeViewModelOutput {
@@ -21,7 +25,26 @@ class HomeViewModel extends BaseViewModel
   // --  inputs
   @override
   void start() {
-    // TODO: implement start
+    _getHomeData();
+  }
+
+  _getHomeData() async {
+    inputState.add(LoadingState(
+        stateRendererType: StateRendererType.fullScreenLoadingState));
+    (await _homeUseCase.execute(Void)).fold(
+        (failure) => {
+              // left -> failure
+              inputState.add(ErrorState(
+                  StateRendererType.fullScreenErrorState, failure.message))
+            }, (homeObject) {
+      // right -> data (success)
+      // content
+      inputState.add(ContentState());
+      inputBanners.add(homeObject.data?.banners);
+      inputServices.add(homeObject.data?.services);
+      inputStores.add(homeObject.data?.stores);
+      // navigate to main screen
+    });
   }
 
   @override
