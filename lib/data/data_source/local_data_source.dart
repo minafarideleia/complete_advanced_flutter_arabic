388311d5cfe8a7de189942073fea528189d744e2
minafarid <mebrahim@elm.sa>
2022-03-19 06:33:59 +0300

    Lecture 157 - Adding Retrieving Caching Data from Local Data Source

--- lib/data/data_source/local_data_source.dart
+++ lib/data/data_source/local_data_source.dart
@@ -1,6 +1,9 @@
+import 'package:advanced_flutter_arabic/data/network/error_handler.dart';
+
 import '../response/responses.dart';
 
 const CACHE_HOME_KEY = "CACHE_HOME_KEY";
+const CACHE_HOME_INTERVAL = 60 * 1000; // 1 minute cache in millis
 
 abstract class LocalDataSource {
   Future<HomeResponse> getHomeData();
@@ -13,9 +16,16 @@ class LocalDataSourceImpl implements LocalDataSource {
   Map<String, CachedItem> cacheMap = Map();
 
   @override
-  Future<HomeResponse> getHomeData() {
-    // TODO: implement getHomeData
-    throw UnimplementedError();
+  Future<HomeResponse> getHomeData() async {
+    CachedItem? cachedItem = cacheMap[CACHE_HOME_KEY];
+
+    if (cachedItem != null && cachedItem.isValid(CACHE_HOME_INTERVAL)) {
+      // return the response from cache
+      return cachedItem.data;
+    } else {
+      // return an error that cache is not there or its not valid
+     throw ErrorHandler.handle(DataSource.CACHE_ERROR);
+    }
   }
 
   @override
@@ -30,4 +40,17 @@ class CachedItem {
   int cacheTime = DateTime.now().millisecondsSinceEpoch;
 
   CachedItem(this.data);
+}
+
+extension CachedItemExtension on CachedItem {
+  bool isValid(int expirationTimeInMillis) {
+    int currentTimeInMillis = DateTime.now().millisecondsSinceEpoch;
+
+    bool isValid = currentTimeInMillis - cacheTime <= expirationTimeInMillis;
+    // expirationTimeInMillis -> 60 sec
+    // currentTimeInMillis -> 1:00:00
+    // cacheTime -> 12:59:30
+    // valid -> till 1:00:30
+    return isValid;
+  }
 }
