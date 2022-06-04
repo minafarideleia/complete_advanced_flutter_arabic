7f586edf3b2d64ca313c600d92d35986e6a53b50
Mohamed AlWhaidi <mo5-98@hotmail.com>
2022-06-04 16:24:24 +0300

    add remote data source

---
+++ lib/data/network/data_source/remote_data_source.dart
@@ -0,0 +1,20 @@
+import 'package:advanced_flutter_arabic/data/network/api_client.dart';
+import 'package:advanced_flutter_arabic/domain/models/responses.dart';
+
+import '../../dto/login.dart';
+
+abstract class RemoteDataSource {
+  Future<Authenticacts> login(LoginDto loginDto);
+}
+
+class RemoteDataSourceImpl implements RemoteDataSource {
+  final ApiClient _apiClient;
+
+  RemoteDataSourceImpl(this._apiClient);
+  @override
+  Future<Authenticacts> login(LoginDto loginDto) async {
+    final response =
+        await _apiClient.login(loginDto.email, loginDto.password, '', '');
+    return response;
+  }
+}
