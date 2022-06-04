40345b97d4a7c1379bbb9ecf2d4a02d30de5c3c5
Mohamed AlWhaidi <mo5-98@hotmail.com>
2022-06-04 19:19:45 +0300

    fix bug - add future return type

--- lib/domain/repository/repositories.dart
+++ lib/domain/repository/repositories.dart
@@ -5,5 +5,5 @@ import 'package:dartz/dartz.dart';
 import '../../data/network/failure.dart';
 
 abstract class Repositories {
-  Either<Failure, Authenticacts> login(LoginDto loginDto);
+  Future<Either<Failure, Authenticacts> > login(LoginDto loginDto);
 }
